ef1dc60ffdd009ea2e204cd0c8bc731f8f96fbbf
Isabella <wileyisabella@gmail.com>
2021-05-26 23:31:19 -0400

    commit tree 28168f257abd8fe69ac077ccb39d53cd4d16b48d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+coupons = [
+    {
+        coupon_code: "SUMMER15",
+        store: "Macy"
+    },
+    {
+        coupon_code: "FALL20",
+        store: "JCPenny",
+    },
+    {
+        coupon_code: "WELCOME25",
+        store: "Forever 21",
+    },
+    {
+        coupon_code: "NEW20",
+        store: "CVS"
+    },
+    {
+        coupon_code: "SPRING15",
+        store: "Kohls"
+    }
+]
+
+coupons.each do |coupon|
+    Coupon.create(coupon)
+end
